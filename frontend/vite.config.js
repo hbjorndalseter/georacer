4e2560241f14df1b229acb5bbbe152c9ce379c37
Åmund <aamgrim@gmail.com>
2025-02-24 13:15:12 +0100

    add tailwind and fix package.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(async () => {
+  const tailwindcss = (await import('@tailwindcss/vite')).default
+  return {
+    root: 'frontend',
+    plugins: [
+      tailwindcss(),
+    ],
+    optimizeDeps: {
+      exclude: ['@tailwindcss/vite']
+    }
+  }
 })
