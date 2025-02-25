438bea1de0d3258200690e1013c78e1c4ef6714f
hjalmabj <hjalmabj@stud.ntnu.no>
2025-02-25 14:21:16 +0100

    changed back to original app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react"
 
 export default function App() {
+
   const [msg, setMsg] = useState('')
 
   useEffect(() => {
-    fetch('http://localhost:3000/api/players') // Endepunktet i backend
+    fetch('http://localhost:3000')
       .then(response => response.json())
-      .then(data => setMsg(data.length > 0 ? `Fant ${data.length} spillere!` : "Ingen spillere funnet."))
-      .catch(error => console.error("Feil ved henting av data:", error));
+      .then(data => setMsg(data.msg))
   }, [])
 
   return (
