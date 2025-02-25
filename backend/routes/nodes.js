bbf44206326f7cb63be82b40c1089d4a64cf6113
Åmund <aamgrim@gmail.com>
2025-02-25 19:14:25 +0100

    check api endpoint node

--- backend/routes/nodes.js
+++ backend/routes/nodes.js
@@ -3,13 +3,18 @@ import prisma from '../db.js';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get('/:cityMapId', async (req, res) => {
     try {
-        const nodes = await prisma.Node.findMany();
+        const nodes = await prisma.node.findMany({
+            where: {
+                cityMapId: parseInt(req.params.cityMapId)
+            }
+        });
+        console.log("Nodes:", nodes)
         res.json(nodes);
     } catch (error) {
         console.error("Feil ved henting av noder:", error);
-        res.status(500).json({ error: 'Noe gikk galt' });
+        res.status(500).json({ error: 'Noe jævlig gikk galt' });
     }
 }
 );
--- frontend/src/pages/PlayPage.jsx
+++ frontend/src/pages/PlayPage.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from 'react';
+
 export default function PlayPage() {
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/node/1')
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error("Error:", error));
+    }
+    , []);
+
     return (
         <div>
             **FYLL INN**
