76c90ae52337000cae42c173ab691adaf7840b87
hjalmabj <hjalmabj@stud.ntnu.no>
2025-03-16 12:53:56 +0100

    set up and cleaned project

--- backend/prisma/migrations/20250313113146_intersection_boolean_node/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Node" ADD COLUMN     "isIntersection" BOOLEAN NOT NULL DEFAULT false;
-
--- AlterTable
-ALTER TABLE "Players" ADD COLUMN     "highscore" INTEGER;
--- frontend/src/pages/PlayPage.jsx
+++ frontend/src/pages/PlayPage.jsx
@@ -1,13 +1,12 @@
-import { useEffect } from 'react';
-import Map from '../components/Map';
+import React from 'react';
 
-export default function PlayPage() {
-
-    const mapId = 1; // Bruker map 1 i denne omgang
-
+const PlayPage = () => {
     return (
-        <div className="w-screen h-screen bg-[#1b325e] justify-center items-center flex flex-col">
-            <Map mapId={mapId} />
+        <div>
+            <h1>Welcome to the Play Page</h1>
+            <p>This is a dummy page for playing around with content.</p>
         </div>
-    )
-}
+    );
+};
+
+export default PlayPage;
