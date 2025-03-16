bcf8af1b2f04bb96657cb4560852b851b97f4462
KristianLarsen <kristiannlarsen11@gmail.com>
2025-03-16 14:31:33 +0100

    added example

--- frontend/src/components/MapComponent.jsx
+++ frontend/src/components/MapComponent.jsx
@@ -1,22 +1,30 @@
 import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import FactQuestion from "../components/FactQuestion";
 
-const MapComponent = () => {
-    return (
-        <MapContainer 
-            center={[63.4305, 10.3951]} 
-            zoom={20} 
-            style={{ height: "100vh", width: "100vw" }} // Full screen
-            zoomControl={false}
-        >
-            <TileLayer
-            url="https://tile.thunderforest.com/pioneer/{z}/{x}/{y}.png?apikey=6e87f65590d044e99e27369fd99280da"
-            attribution='Maps &copy; <a href="https://www.thunderforest.com/">Thunderforest</a>, 
-                        Data &copy; <a href="http://www.openstreetmap.org/copyright">
-                        OpenStreetMap</a>'
-            />
-        </MapContainer>
-    );
+const MapComponent = ({ handleCorrectAnswer }) => {
+  return (
+    <MapContainer 
+      center={[63.4305, 10.3951]} 
+      zoom={20} 
+      style={{ height: "100vh", width: "100vw" }} // Full screen
+      zoomControl={false}
+    >
+      <TileLayer
+        url="https://tile.thunderforest.com/pioneer/{z}/{x}/{y}.png?apikey=6e87f65590d044e99e27369fd99280da"
+        attribution='Maps &copy; <a href="https://www.thunderforest.com/">Thunderforest</a>, 
+                     Data &copy; <a href="http://www.openstreetmap.org/copyright">
+                     OpenStreetMap</a>'
+      />
+      
+      <FactQuestion
+        coords={[63.429, 10.393]} // Coordinates for the sculpture
+        question="Who created this sculpture?"
+        correctAnswer="Gustav Vigeland" // Expected answer (case-insensitive)
+        onCorrectAnswer={handleCorrectAnswer}
+      />
+    </MapContainer>
+  );
 };
 
 export default MapComponent;
--- frontend/src/pages/PlayPage.jsx
+++ frontend/src/pages/PlayPage.jsx
@@ -1,11 +1,34 @@
+import { useState } from "react";
 import MapComponent from "../components/MapComponent";
 
 const PlayPage = () => {
-    return (
-        <div>
-            <MapComponent />
-        </div>
-    );
+  const [score, setScore] = useState(0);
+
+  const handleCorrectAnswer = () => {
+    setScore((prevScore) => prevScore + 1);
+  };
+
+  return (
+    <div style={{ position: "relative", width: "100vw", height: "100vh" }}>
+      {/* Score display */}
+      <div
+        style={{
+          position: "absolute",
+          top: 10,
+          left: 10,
+          zIndex: 9999,
+          backgroundColor: "white",
+          padding: "5px 10px",
+          borderRadius: "4px",
+        }}
+      >
+        Score: {score}
+      </div>
+
+      {/* Pass handleCorrectAnswer to MapComponent */}
+      <MapComponent handleCorrectAnswer={handleCorrectAnswer} />
+    </div>
+  );
 };
 
 export default PlayPage;
