a3a9377f2c9246cc82b925f96b98b552d70c2d3d
hjalmabj <hjalmabj@stud.ntnu.no>
2025-03-17 11:49:26 +0100

    reset database

--- backend/prisma/migrations/20250316125758_player_question_relation/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `score` to the `player` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "player" ADD COLUMN     "completed_question" INTEGER[],
-ADD COLUMN     "score" INTEGER NOT NULL;
