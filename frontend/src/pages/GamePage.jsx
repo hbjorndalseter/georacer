792ec514cf5c3219830d7f3e49bfde71789688a7
Åmund <aamgrim@gmail.com>
2025-03-24 13:49:30 +0100

    Navigate to resultpage when finished

--- frontend/src/pages/GamePage.jsx
+++ frontend/src/pages/GamePage.jsx
@@ -84,7 +84,7 @@ export default function GamePage() {
       } else {
         await updateScore(player, newScore);
         updatePlayerScore(newScore);
-        navigate("/");
+        navigate("/Result");
       }
     } else {
       alert("Incorrect answer. Try again!");
