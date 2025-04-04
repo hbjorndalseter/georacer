af46728ba2fa2c5a71a57736825c05bfc8d75872
Åmund <aamgrim@gmail.com>
2025-04-04 12:26:54 +0200

    Cleaner arrow animation

--- frontend/src/components/Arrow.jsx
+++ frontend/src/components/Arrow.jsx
@@ -5,11 +5,18 @@ import arrowURL from '../assets/arrowScaledDown.png'
 
 export default function Arrow({ rotation_angle, x_position, y_position, onClick }) {
     return (
+      <div
+        className="arrowWrapper"
+        style={{
+          transform: `translate(${x_position}px, ${-y_position}px) rotate(${rotation_angle}deg)`,
+          transformOrigin: 'top left',
+        }}
+      >
         <img
-            className="arrowSprite"
-            style={{ transform: `translate(${x_position}px, ${-y_position}px) rotate(${rotation_angle}deg)`}}
-            onClick={onClick}
-            src={arrowURL}
-        ></img>
+          src={arrowURL}
+          className="arrowSprite arrow-grow"
+          onClick={onClick}
+        />
+      </div>
     );
-}
+  }
