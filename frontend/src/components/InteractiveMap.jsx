a0118d74e2e3ed6dc984a7c0b60a76f27f81d688
Åmund <aamgrim@gmail.com>
2025-04-22 14:23:33 +0200

    New basemap from mapbox

--- frontend/src/components/InteractiveMap.jsx
+++ frontend/src/components/InteractiveMap.jsx
@@ -83,12 +83,12 @@ export default function InteractiveMap({ mapId, checkpointNode, onCheckpointReac
         const offset = 90;
         const prev = normalize(carRotation);
         const target = normalize(rawRotation + offset);
-      
+
         let delta = target - prev;
-      
+
         if (delta > 180) delta -= 360;
         if (delta < -180) delta += 360;
-      
+
         const final = carRotation + delta;
         setCarRotation(final);
 
@@ -112,9 +112,13 @@ export default function InteractiveMap({ mapId, checkpointNode, onCheckpointReac
                 zoomControl={false}
                 doubleClickZoom={false}
             >
-                <TileLayer
+                {/* <TileLayer
                     url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
                     attribution="&copy; OpenStreetMap & CartoDB"
+                /> */}
+                <TileLayer
+                    url="https://api.mapbox.com/styles/v1/aamgrim/cm9sh59tt00ko01s52eqof5f8/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYWFtZ3JpbSIsImEiOiJjbTlzY3d3ZXMwMTV1Mm1zZ3p4NTR2cWc1In0.4u2LSXOHO1_GvcVEjrVTTg"
+                    attribution="&copy; Mapbox & OpenStreetMap contributors"
                 />
                 <CenterMap />
                 {checkpointNode && (
